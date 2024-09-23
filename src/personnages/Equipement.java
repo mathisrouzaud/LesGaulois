edbcba479c940c37f8ca82f0337ce6c4bdaaeffb
mathisrouzaud <115712581+mathisrouzaud@users.noreply.github.com>
2024-09-23 19:51:23 +0200

    fin

---
+++ src/personnages/Equipement.java
@@ -0,0 +1,21 @@
+package personnages;
+
+public enum Equipement {
+    CASQUE("casque"),
+    BOUCLIER("bouclier");
+
+    private final String nom;
+
+    Equipement(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+    private Equipement[] equipements = new Equipement[2];
+    private int nbEquipements = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -20,6 +22,33 @@ public class Romain {
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
+	
+    public void sEquiper(Equipement equipement) {
+        switch (nbEquipements) {
+            case 0:
+            case 1:
+                if (contientDeja(equipement)) {
+                    System.out.println("Le soldat " + nom + " possède déjà un " + equipement + "!");
+                } else {
+                    equipements[nbEquipements] = equipement;
+                    nbEquipements++;
+                    System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+                }
+                break;
+            case 2:
+                System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+                break;
+        }
+    }
+
+    private boolean contientDeja(Equipement equipement) {
+        for (Equipement equipementActuel : equipements) {
+            if (equipementActuel != null && equipementActuel.equals(equipement)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 	public void recevoirCoup(int forceCoup) {
 		force -= forceCoup;
@@ -29,4 +58,11 @@ public class Romain {
 			parler("J'abandonne...");
 		}
 	}
+    public static void main(String[] args) {
+       Romain minus = new Romain("Minus",4);
+       minus.sEquiper(Equipement.CASQUE);
+       minus.sEquiper(Equipement.CASQUE);
+       minus.sEquiper(Equipement.BOUCLIER);
+       minus.sEquiper(Equipement.BOUCLIER);
+    }
 }
